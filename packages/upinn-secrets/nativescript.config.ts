e0e6d41741515f512577202267b3b709326da043
wuilmer <wuilmermorgado24@gmail.com>
2025-11-18 15:10:18 -0500

    Finish: ios

--- packages/upinn-secrets/nativescript.config.ts
+++ packages/upinn-secrets/nativescript.config.ts
@@ -1,14 +1,14 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-    ios: {
-        SPMPackages: [
-            {
-                name: 'UpinnSecretsiOSLib',
-                libs: ['UpinnSecretsiOSLib'],
-                repositoryURL: 'https://github.com/upinn29/upinnsecretsios.git',
-                version: '1.3.2',
-            },
-        ],
-    },
+  ios: {
+    SPMPackages: [
+      {
+        name: 'UpinnSecretsiOSLib',
+        libs: ['UpinnSecretsiOSLib'],
+        repositoryURL: 'https://github.com/upinn29/upinnsecretsios.git',
+        version: '1.3.3',
+      },
+    ],
+  },
 } as NativeScriptConfig;
